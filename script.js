0d612690126aa22045be303bf657b315116223bd
AgentBLOX11 <107680333+AgentBLOX11@users.noreply.github.com>
2023-03-12 04:15:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let barsEL = document.querySelector( Selectiors= '.bars');
-let iconBarsEl = document.querySelector(Selectiors='.icon-bars');
-
-barsEL.addEventListener( type='click', listener= () =>{
-    iconBarsEl.classList.toggle( token='show');
-});
