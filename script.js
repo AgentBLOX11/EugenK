5589799349843cd858458cfff67932311a44afee
AgentBLOX11 <107680333+AgentBLOX11@users.noreply.github.com>
2023-03-12 04:10:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let barsEL = document.querySelector( Selectiors= '.bars');
+let iconBarsEl = document.querySelector(Selectiors='.icon-bars');
+
+barsEL.addEventListener( type='click', listener= () =>{
+    iconBarsEl.classList.toggle( token='show');
+});
